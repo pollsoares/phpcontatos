335345735c7952feb8de9535d3f3fb0d2b4cd7c7
pollsoares <soares.si@gmail.com>
2023-02-07 09:30:49 -0300

    commit inicial

---
+++ gerarPDF.php
@@ -0,0 +1,44 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+include("conexao.php");
+require_once __DIR__ . '/vendor/autoload.php';
+
+$grupo = selectAllPessoa();
+$mpdf = new \Mpdf\Mpdf();
+$mpdf->SetDisplayMode("fullpage");
+$mpdf->WriteHTML("<h1>Relátorio de dados Agenda</h1><hr/>");
+
+$html = "<table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Nascimento</th>
+                    <th>Telefone</th>
+                    <th>Endereço</th>
+                </tr>
+            </thead>
+            <tbody>";
+                    foreach ($grupo as $pessoa) {
+                
+                $html = $html ."    <tr>
+                    <td>{$pessoa["id"]}</td>
+                    <td>{$pessoa["nome"]}</td>
+                    <td>".formatoData($pessoa["nascimento"])."</td>
+                    <td>{$pessoa["telefone"]}</td>
+                    <td>{$pessoa["endereco"]}</td>
+                     </tr>";
+                 }
+               
+          $html = $html ."  </tbody>
+        </table>";
+$mpdf->WriteHTML($html);
+$mpdf->Output();
+exit();
+
